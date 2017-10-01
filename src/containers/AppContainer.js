495eea0bf8a77ee512d962dc618dcea10144cdc9
Marcin Moch <marcio.moch@gmail.com>
2017-10-01 16:17:27 +0200

    Created folders/modules structure

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import App from '../components/App';
+
+class AppContainer extends PureComponent {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+export default AppContainer;
