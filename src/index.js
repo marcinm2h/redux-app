1f31f3224eb9b1778544c4258fc1ee71ab2561c1
Marcin Moch <marcio.moch@gmail.com>
2017-10-01 19:37:08 +0200

    Configured react-router to work with redux store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import { Provider } from 'react-redux';
 
 import App from './containers/App';
 import reducer from './reducers';
 
+// import { push } from 'react-router-redux';
+// store.dispatch(push('/foo'))
+
+const history = createHistory();
+
 const middleware = [
+  routerMiddleware(history),
   thunk,
 ];
 
@@ -19,6 +28,8 @@ const store = createStore(
 
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Route exact path="/" component={App} />
+    </ConnectedRouter >
   </Provider>
 ), document.getElementById('root'));
