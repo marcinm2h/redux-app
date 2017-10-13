fe25c1825ac4d9c0c1657f9d8070acf7174a7cae
Marcin Moch <marcio.moch@gmail.com>
2017-10-13 21:53:32 +0200

    Created api snippet/ POC

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,41 @@
+import { youtube } from 'googleapis';
+
+const api = youtube('v3');
+const YT_API_KEY = process.env.YT_API_KEY;
+
+// const query = query => new Promise((resolve, reject) => {
+//   api.search.list({
+//     auth: YT_API_KEY,
+//     part: 'snippet',
+//     q: query,
+//   }, (error, data) => {
+//     if (error) {
+//       reject(error);
+//     } else {
+//       resolve(data);
+//     }
+//   });
+// });
+
+const promisify = fn => params => new Promise((resolve, reject) => {
+  fn({
+    auth: YT_API_KEY,
+    ...params,
+  }, (error, data) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(data);
+    }
+  });
+});
+
+const search = promisify(api.search.list);
+
+
 const sendRequest = async (url, options) => {
+  const q = await search('redux');
+  console.log(q);
   const response = await fetch(url, options);
   if (response.ok) {
     const data = await response.json();
@@ -11,3 +48,14 @@ const sendRequest = async (url, options) => {
 const getCharacter = characterId => sendRequest(`https://swapi.co/api/people/${characterId}/`);
 
 export default { getCharacter };
+
+// const { youtube } = require('googleapis');
+// const yt = youtube('v3');
+
+// const resp = yt.search.list({
+//   auth: API_KEY,
+//   part: 'snippet',
+//   q: 'Dan Abramov'
+// }, (error, response) => {
+//   console.log(response);
+// });
